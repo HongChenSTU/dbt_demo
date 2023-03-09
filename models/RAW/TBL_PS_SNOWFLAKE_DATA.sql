a418fc52a6746229b6cdc8dbea24036fb9e81470
marc.an-voort@adesso.de <marc.an-voort@adesso.de>
2023-03-09 09:16:52 +0000

    commit

--- models/RAW/TBL_PS_SNOWFLAKE_DATA.sql
+++ models/RAW/TBL_PS_SNOWFLAKE_DATA.sql
@@ -7,12 +7,13 @@
 
 with source_data as (
 
-    select d_date,
-    sum(ss_quantity) as sum_ss_quantity,
-    sum(ss_ext_sales_price) as sum_ss_ext_sales_price
+    select d_date
+    ,sum(ss_quantity) as sum_ss_quantity
+    ,sum(ss_ext_sales_price) as sum_ss_ext_sales_price
+    ,DSS_LOAD_DATE
     from {{ ref('TBL_NEW_DEMO_DATA') }} ts
     join snowflake_sample_data.tpcds_sf10tcl.date_dim dd on ts.ss_sold_date_sk = dd.d_date_sk
-    group by d_date
+    group by d_date, DSS_LOAD_DATE
     order by d_date desc
 )
 
